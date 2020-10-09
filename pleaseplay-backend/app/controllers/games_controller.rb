b43e63ad27f957a05ca986d2177c8a682923c719
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-10-08 23:13:06 -0700

    creates show action

--- pleaseplay-backend/app/controllers/games_controller.rb
+++ pleaseplay-backend/app/controllers/games_controller.rb
@@ -1,2 +1,11 @@
 class GamesController < ApplicationController
+
+  def show
+    game = Game.find_by(id: params[:id])
+    render json: { title: game.title,
+    image: game.image,
+    review: game.review,
+    comments: game.comments.find_by(game_id: params[:id]) }
+  end 
+
 end
