96bc9010d23c93410ba28c883bbda37f749e39ba
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-10-11 01:04:20 -0700

    adds dataset.id className to game div

--- pleaseplay-frontend/src/games/Game.js
+++ pleaseplay-frontend/src/games/Game.js
@@ -13,7 +13,7 @@ class Game {
   renderGameDiv(){
     const div = document.getElementById('game-div')
     const gameDiv = document.createElement('DIV')
-    gameDiv.className = 'game'
+    gameDiv.dataset.id = this.game.id
     div.appendChild(gameDiv)
 
     const gameTitle = document.createElement('H3')
@@ -21,13 +21,13 @@ class Game {
     gameDiv.appendChild(gameTitle)
 
     const gameImage = document.createElement('IMG')
+    gameImage.className = 'game-img'
     gameImage.src = this.game.image
     gameDiv.appendChild(gameImage)
 
     const gameReview = document.createElement('P')
     gameReview.innerHTML = this.game.review
     gameDiv.appendChild(gameReview)
-
   }
 
   // rednerGameData(){
