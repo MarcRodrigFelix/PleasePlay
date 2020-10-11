203aae378fc950405bca1b8f77c6b1e21de7820a
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-10-11 14:41:27 -0700

    renderGameDiv() deconstructs game json data

--- pleaseplay-frontend/src/games/Game.js
+++ pleaseplay-frontend/src/games/Game.js
@@ -11,27 +11,26 @@ class Game {
   }
 
   renderGameDiv(){
+    const { title, image, review } = this.game
     const div = document.getElementById('game-div')
+
     const gameDiv = document.createElement('DIV')
     gameDiv.dataset.id = this.game.id
     div.appendChild(gameDiv)
-
+// create div for game
     const gameTitle = document.createElement('H3')
-    gameTitle.innerHTML = this.game.title
+    gameTitle.innerHTML = title
     gameDiv.appendChild(gameTitle)
-
+// create title for game 
     const gameImage = document.createElement('IMG')
     gameImage.className = 'game-img'
-    gameImage.src = this.game.image
+    gameImage.src = image
     gameDiv.appendChild(gameImage)
-
+// create paragraph for game review
     const gameReview = document.createElement('P')
-    gameReview.innerHTML = this.game.review
+    gameReview.innerHTML = review
     gameDiv.appendChild(gameReview)
   }
 
-  // rednerGameData(){
-  //   const { title, image, review } = this.game
-  // }
 
 }
